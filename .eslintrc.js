554ddc6542209439515feb0237b560f210e5c98e
Alolazz <aly_nabil@hotmail.com>
2025-06-05 01:10:10 +0200

    varcel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
+  root: true,
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
